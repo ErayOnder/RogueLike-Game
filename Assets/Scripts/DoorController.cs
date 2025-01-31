aa2bdb850070c78f5cb605f92b6f4058f6bc2000
ErayOnder <erayonder21@ku.edu.tr>
2025-01-31 11:52:09 +0300

    Door added to HallLayout, open door functionality implemented

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,34 @@
+using System.Numerics;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class DoorController : MonoBehaviour
+{
+    public Tilemap tilemap;
+    public Tile openDoorTile;
+    private Vector3Int doorPosition = new Vector3Int(1, -8, 0);
+    private bool isLocked = true;
+
+    [ContextMenu("Unlock Door")]
+    public void Unlock()
+    {
+        if (isLocked)
+        {
+            isLocked = false;
+            tilemap.SetTile(doorPosition, openDoorTile);
+            GetComponent<TilemapCollider2D>().enabled = false;
+            Debug.Log("Door unlocked!");
+        }
+        else
+        {
+            Debug.Log("Door is already unlocked!");
+        }
+    }
+
+    public bool IsLocked()
+    {
+        return isLocked;
+    }
+    
+}
